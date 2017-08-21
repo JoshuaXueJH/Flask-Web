77fb388caf7765c1881486f53658b60a8af5543e
joshua <xue918715582@126.com>
2017-08-21 09:12:56 +0800

    14 added

---
+++ 14/app/api_1_0/posts.py
@@ -0,0 +1,33 @@
+from flask import request, current_app, g, abort, url_for, jsonify
+from .. import db
+from ..models import Post, Permission
+from . import api
+from .decorators import permission_required
+from .errors import forbidden
+
+
+@api.route('/posts/')
+def get_posts():
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page - 1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page + 1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
+
+
+@api.route('/posts/<int:id>')
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
