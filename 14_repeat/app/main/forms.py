e6350d6146d8fa9f6654336dc00cbc76796db28c
joshua <xue918715582@126.com>
2017-10-15 20:58:31 +0800

    in the middle of profile_edit

--- 14_repeat/app/main/forms.py
+++ 14_repeat/app/main/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, TextAreaField
+from wtforms import SubmitField, StringField, TextAreaField, BooleanField, SelectField
 from flask_pagedown.fields import PageDownField
-from wtforms.validators import Required
+from wtforms.validators import Required, Length, Email, Regexp
 
 
 class PostForm(FlaskForm):
     post = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[a-zA-Z][a-zA-Z0-9_.]*$'), 0,
+                                                   'Username must have only letters, numbers, dots or underlines'])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+    
--- 14_repeat/app/main/views.py
+++ 14_repeat/app/main/views.py
@@ -1,8 +1,8 @@
 from . import main
-from flask import render_template, redirect, url_for
-from flask_login import current_user
-from .forms import PostForm
-from ..models import Permission, Post
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_required
+from .forms import PostForm, EditProfileForm
+from ..models import Permission, Post, User
 from .. import db
 
 
@@ -14,3 +14,49 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
     return render_template('index.html', form=form)
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
+
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+@main.route('/edit_profile/<int:id>')
+@login_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form =
+
+
+@main.route('/followers/<username>')
+def followers(username):
+    user = User.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+
+
+@main.route('/followed_by/<username>')
+def followed_by(username):
+    user = User.filter_by(username=username)
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
